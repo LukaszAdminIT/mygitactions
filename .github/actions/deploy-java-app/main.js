313ef351bdcbc44be4412e19bcc4808d1d5f126a
ŁukaszRomanik <134381579+LukaszAdminIT@users.noreply.github.com>
2025-07-03 14:19:32 +0200

    ci cd try 1

--- .github/actions/deploy-java-app/main.js
+++ .github/actions/deploy-java-app/main.js
@@ -3,6 +3,13 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket',{ required: true});
+    const bucketRegion = core.getInput('bucket-region',{ required: true});
+    const distFolder = core.getInput('dist-folder',{ required: true});
+
+    // 2) Upload files
+    exec.exec('echo ""')
     core.notice('Hello from my custom JavaScript Action!')
 }
 
